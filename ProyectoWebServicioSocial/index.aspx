78b615d570ecb23efaabc66f5efb4ece047f18aa
ZackMaya <ingsoftnetozack@outlook.com>
2018-09-14 19:26:32 -0500

    Subida de Archivos

---
+++ ProyectoWebServicioSocial/index.aspx
@@ -0,0 +1,69 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="ProyectoWebServicioSocial.index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            height: 92px;
+        }
+    </style>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <center>
+    <h1>Prueba de desarrollo Web </h1>
+    
+    <h5>Elabora: Ernesto Z. Maya D.</h5>
+    
+
+    <form id="form1" runat="server">
+        <table style="max-width:70%" >
+            <tr>
+                <td class="auto-style1">Selecciona el filtro que deseas</td>
+            </tr>
+                <tr>
+                <td class="auto-style1">
+                    <asp:Button Text="Precio ↑" ID="btnpreciodesc" runat="server" OnClick="btnpreciodesc_Click1" class="btn" />
+                    <asp:Button Text="Precio ↓" ID="btnprecioasc" runat="server" OnClick="btnprecioasc_Click" class="btn" />
+                </td>
+                <td class="auto-style1">
+                    <asp:Button Text="Existencia ↑" ID="btnexistenciadesc" runat="server" OnClick="btnexistenciadesc_Click" class="btn"/>
+                    <asp:Button Text="Existencia ↓" ID="btnexistenciaasc" runat="server" OnClick="btnexistenciaasc_Click" class="btn" />
+                </td>
+                <td class="auto-style1">
+                    <asp:Button Text="A-Z ↑" ID="btnalfadesc" runat="server" OnClick="btnalfadesc_Click" class="btn"/>
+                    <asp:Button Text="A-Z ↓" ID="btnalfaasc" runat="server" OnClick="btnalfaasc_Click" class="btn"/>
+                </td>
+                </tr>
+        </table>
+
+        <br />
+        <hr />
+        <h3>Lista de Productos</h3>
+        <br />
+    <div>
+        <table class="table table-bordered">
+            <asp:GridView ID="getProducto" runat="server" AutoGenerateColumns="false" Width="806px">
+                <Columns>
+                    <asp:BoundField DataField="nombre_producto" HeaderText="Nombre Producto" />
+                    <asp:BoundField DataField="precio" HeaderText="Precio MX" />
+                    <asp:BoundField DataField="nombre_categoria" HeaderText="Categoria" />
+                    <asp:BoundField DataField="cantidad_actual" HeaderText="Existencia" />
+                    <asp:BoundField DataField="descripcion_producto" HeaderText="Descripción" />
+
+                </Columns>
+            </asp:GridView>
+            </table>
+    </div>
+    </form>
+        <br />
+        <br />
+        <br />
+        </center>
+</body>
+</html>
---
+++ ProyectoWebServicioSocial/index.aspx.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+//Espacio de nombres para conección
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace ProyectoWebServicioSocial
+{
+    public partial class index : System.Web.UI.Page
+    {
+        //Conección MySQL
+        string connectionString = @"Server=localhost;Database=listaproductos;Uid=root;Pwd=root;";
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            GridFill();
+          
+
+        }
+
+        //Método mostrar lista de prodcutos a través de Grill
+        void GridFill()
+        {
+            using (MySqlConnection sqlConexion = new MySqlConnection(connectionString))
+            {
+                sqlConexion.Open();
+                MySqlDataAdapter sqlProduct = new MySqlDataAdapter("ver_productos",sqlConexion);
+                sqlProduct.SelectCommand.CommandType = System.Data.CommandType.StoredProcedure;
+                DataTable datatable = new DataTable();
+                sqlProduct.Fill(datatable);
+                getProducto.DataSource = datatable;
+                getProducto.DataBind();
+            }
+        }
+
+        //Métodos para usar los filtros y ejecutar los Join en las base de datos
+        //Se podría mejorar y optimizar el código usando un combo box
+
+
+        protected void btnpreciodesc_Click1(object sender, EventArgs e)
+        {
+            using (MySqlConnection sqlConexion = new MySqlConnection(connectionString))
+            {
+                sqlConexion.Open();
+                MySqlDataAdapter sqlProduct = new MySqlDataAdapter("precio_desc", sqlConexion);
+                sqlProduct.SelectCommand.CommandType = System.Data.CommandType.StoredProcedure;
+                DataTable datatable = new DataTable();
+                sqlProduct.Fill(datatable);
+                getProducto.DataSource = datatable;
+                getProducto.DataBind();
+            }
+        }
+
+        protected void btnprecioasc_Click(object sender, EventArgs e)
+        {
+            using (MySqlConnection sqlConexion = new MySqlConnection(connectionString))
+            {
+                sqlConexion.Open();
+                MySqlDataAdapter sqlProduct = new MySqlDataAdapter("precio_asc", sqlConexion);
+                sqlProduct.SelectCommand.CommandType = System.Data.CommandType.StoredProcedure;
+                DataTable datatable = new DataTable();
+                sqlProduct.Fill(datatable);
+                getProducto.DataSource = datatable;
+                getProducto.DataBind();
+            }
+
+        }
+
+        protected void btnexistenciadesc_Click(object sender, EventArgs e)
+        {
+            using (MySqlConnection sqlConexion = new MySqlConnection(connectionString))
+            {
+                sqlConexion.Open();
+                MySqlDataAdapter sqlProduct = new MySqlDataAdapter("exis_desc", sqlConexion);
+                sqlProduct.SelectCommand.CommandType = System.Data.CommandType.StoredProcedure;
+                DataTable datatable = new DataTable();
+                sqlProduct.Fill(datatable);
+                getProducto.DataSource = datatable;
+                getProducto.DataBind();
+            }
+
+        }
+
+        protected void btnexistenciaasc_Click(object sender, EventArgs e)
+        {
+            using (MySqlConnection sqlConexion = new MySqlConnection(connectionString))
+            {
+                sqlConexion.Open();
+                MySqlDataAdapter sqlProduct = new MySqlDataAdapter("exis_asc", sqlConexion);
+                sqlProduct.SelectCommand.CommandType = System.Data.CommandType.StoredProcedure;
+                DataTable datatable = new DataTable();
+                sqlProduct.Fill(datatable);
+                getProducto.DataSource = datatable;
+                getProducto.DataBind();
+            }
+
+        }
+
+        protected void btnalfadesc_Click(object sender, EventArgs e)
+        {
+            using (MySqlConnection sqlConexion = new MySqlConnection(connectionString))
+            {
+                sqlConexion.Open();
+                MySqlDataAdapter sqlProduct = new MySqlDataAdapter("alfa_desc", sqlConexion);
+                sqlProduct.SelectCommand.CommandType = System.Data.CommandType.StoredProcedure;
+                DataTable datatable = new DataTable();
+                sqlProduct.Fill(datatable);
+                getProducto.DataSource = datatable;
+                getProducto.DataBind();
+            }
+
+        }
+
+        protected void btnalfaasc_Click(object sender, EventArgs e)
+        {
+            using (MySqlConnection sqlConexion = new MySqlConnection(connectionString))
+            {
+                sqlConexion.Open();
+                MySqlDataAdapter sqlProduct = new MySqlDataAdapter("alfa_asc", sqlConexion);
+                sqlProduct.SelectCommand.CommandType = System.Data.CommandType.StoredProcedure;
+                DataTable datatable = new DataTable();
+                sqlProduct.Fill(datatable);
+                getProducto.DataSource = datatable;
+                getProducto.DataBind();
+            }
+        }
+
+     
+
+    }
+}
